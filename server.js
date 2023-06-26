717d2be9bcb7586545461083514879923d9027e0
Marie Kagermanova <maria.kgm04@gmail.com>
2023-06-26 03:05:48 +0300

    feat: complete nwd tasks

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import { homedir } from "os";
-import { chdir } from "process";
+import { chdir, cwd } from "process";
+import { resolve, join } from "path";
+import { access, readdir } from "fs";
 
 const userHomeDir = homedir();
 chdir(userHomeDir);
@@ -10,7 +12,7 @@ username.splice(0, 1, argument[0].toUpperCase());
 username = username.join("");
 
 console.log(`Welcome to the File Manager, ${username}!`);
-console.log(`You are currently in ${userHomeDir}`);
+displayCurrentDirectory(userHomeDir);
 
 process.on("exit", () => console.log(`Thank you for using File Manager, ${username}, goodbye!`));
 process.on("SIGINT", () => {
@@ -24,4 +26,53 @@ process.on("SIGINT", () => {
 //   }
 // });
 
-process.stdin.on("data", () => {});
+process.stdin.on("data", (input) => {
+  const command = input.toString().replaceAll(/\s+/g, " ");
+
+  if (command.startsWith("up")) {
+    const upPath = join(cwd(), "../");
+    const currentPath = cwd()
+      .split("\\")
+      .filter((item) => item !== "");
+
+    if (currentPath.length !== 1) {
+      chdir(upPath);
+      displayCurrentDirectory(upPath);
+    }
+  } else if (command.startsWith("cd")) {
+    const path = command.split(" ")[1];
+    const fullPath = resolve(path);
+
+    access(fullPath, (err) => {
+      if (!err) {
+        chdir(fullPath);
+        displayCurrentDirectory(fullPath);
+      } else {
+        displayError("invalid_input");
+      }
+    });
+  } else if (command.startsWith("ls")) {
+    const currentDir = cwd();
+    const tableData = [];
+
+    readdir(currentDir, { withFileTypes: true }, (err, data) => {
+      data.map((item) =>
+        tableData.push({ Name: item.name, Type: item.isFile() ? "file" : "directory" })
+      );
+
+      console.table(tableData);
+    });
+  }
+});
+
+function displayCurrentDirectory(currentDirectory) {
+  console.log(`You are currently in ${currentDirectory}`);
+}
+
+function displayError(err) {
+  if (err === "invalid_input") {
+    console.log("Invalid input");
+  } else if (err === "operation_failed") {
+    console.log("Operation failed");
+  }
+}
